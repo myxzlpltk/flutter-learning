4ad53bd1e5b5bc39e32dc4f2fa3ff3be01ad7e06
Saddam Azy <myxzlpltk@gmail.com>
2020-12-18 17:58:36 +0700

    Learning anonymous method

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int number = 0;
-
-  void counterUp() {
-    setState(() {
-      number = number + 1;
-    });
-  }
+  String message = "Ini adalah Text";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Counter"),
+          title: Text("Anonymous Method"),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                number.toString(),
-                style: TextStyle(
-                  fontSize: 14+number.toDouble()
-                ),
-              ),
+            children: [
+              Text(message),
               RaisedButton(
-                child: Text("Tambah"),
-                onPressed: counterUp,
+                child: Text("Tekan Saya"),
+                onPressed: () {
+                  setState(() {
+                    message = "Tombol sudah ditekan";
+                  });
+                },
               )
             ],
           ),
