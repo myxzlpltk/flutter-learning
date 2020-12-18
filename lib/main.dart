46515f29ea2be3c630899bd5321e5828b710fbcd
Saddam Azy <myxzlpltk@gmail.com>
2020-12-18 23:13:56 +0700

    Learning stack

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,91 @@
-import 'dart:async';
-import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  List<Widget> widgets = [];
-  int counter = 1;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    Timer.periodic(new Duration(seconds: 1), (timer) {
-      setState(() {
-
-      });
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Random random = Random();
-
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: Text("Latihan Animated Container"),
+            title: Text("Latihan Stack dan Align"),
           ),
-          body: Center(
-            child: GestureDetector(
-              onTap: (){
-                setState(() {
-
-                });
-              },
-              child: AnimatedContainer(
-                color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
-                duration: Duration(seconds: 1),
-                width: 50.0 + random.nextInt(101),
-                height: 50.0 + random.nextInt(101),
+          body: Stack(
+            children: [
+              // Background
+              Column(
+                children: [
+                  Flexible(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.cyan,
+                              )),
+                          Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.red,
+                              ))
+                        ],
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.yellow,
+                              )),
+                          Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.black,
+                              ))
+                        ],
+                      )),
+                ],
               ),
-            ),
-          )
-      ),
+              // ListView
+              ListView(
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        "Ini adalah teks yang ada di lapisan tengah dari Stack.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      Text(
+                        "Ini adalah teks yang ada di lapisan tengah dari Stack.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      Text(
+                        "Ini adalah teks yang ada di lapisan tengah dari Stack.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              // Button di Tengah
+              Align(
+                alignment: Alignment(0, 0.9),
+                child: RaisedButton(
+                  onPressed: (){},
+                  child: Text("My Button"),
+                  color: Colors.blue,
+                ),
+              )
+            ],
+          )),
     );
   }
 }
