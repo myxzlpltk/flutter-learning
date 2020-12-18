8c419f9d870d1d3e213475e26b3c070d552cd1b4
Saddam Azy <myxzlpltk@gmail.com>
2020-12-18 17:27:13 +0700

    Learning stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,45 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+
+  void counterUp() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Learning Container Widget")),
-        body: Container(
-          color: Colors.red,
-          margin: EdgeInsets.all(10),
-          child: Container(
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: <Color>[
-                  Colors.blue,
-                  Colors.pink
-                ]
+        appBar: AppBar(
+          title: Text("Counter"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                number.toString(),
+                style: TextStyle(
+                  fontSize: 14+number.toDouble()
+                ),
+              ),
+              RaisedButton(
+                child: Text("Tambah"),
+                onPressed: counterUp,
               )
-            ),
+            ],
           ),
-        )
+        ),
       ),
     );
   }
-
 }
