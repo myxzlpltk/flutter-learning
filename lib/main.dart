cc2f820adc5c6781e3f59e1b180ad8a03548ab9d
Saddam Azy <myxzlpltk@gmail.com>
2020-12-19 00:43:40 +0700

    Learning image

--- lib/main.dart
+++ lib/main.dart
@@ -9,83 +9,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Latihan Stack dan Align"),
+        appBar: AppBar(title: Text("Latihan Image"),),
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            padding: EdgeInsets.all(3),
+            child: Image(
+              image: AssetImage("assets/juan.jpg"),
+              fit: BoxFit.cover,
+              repeat: ImageRepeat.noRepeat
+            ),
           ),
-          body: Stack(
-            children: [
-              // Background
-              Column(
-                children: [
-                  Flexible(
-                      flex: 1,
-                      child: Row(
-                        children: [
-                          Flexible(
-                              flex: 1,
-                              child: Container(
-                                color: Colors.cyan,
-                              )),
-                          Flexible(
-                              flex: 1,
-                              child: Container(
-                                color: Colors.red,
-                              ))
-                        ],
-                      )),
-                  Flexible(
-                      flex: 1,
-                      child: Row(
-                        children: [
-                          Flexible(
-                              flex: 1,
-                              child: Container(
-                                color: Colors.yellow,
-                              )),
-                          Flexible(
-                              flex: 1,
-                              child: Container(
-                                color: Colors.black,
-                              ))
-                        ],
-                      )),
-                ],
-              ),
-              // ListView
-              ListView(
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        "Ini adalah teks yang ada di lapisan tengah dari Stack.",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 30),
-                      ),
-                      Text(
-                        "Ini adalah teks yang ada di lapisan tengah dari Stack.",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 30),
-                      ),
-                      Text(
-                        "Ini adalah teks yang ada di lapisan tengah dari Stack.",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              // Button di Tengah
-              Align(
-                alignment: Alignment(0, 0.9),
-                child: RaisedButton(
-                  onPressed: (){},
-                  child: Text("My Button"),
-                  color: Colors.blue,
-                ),
-              )
-            ],
-          )),
+        ),
+      ),
     );
   }
 }
+
