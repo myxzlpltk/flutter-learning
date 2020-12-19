41334309fbf9cc55a562944252b19cd8918ce976
Saddam Azy <myxzlpltk@gmail.com>
2020-12-20 01:04:38 +0700

    Learning MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -4,38 +4,24 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController controller = TextEditingController(text: "Nilai Awal");
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Learning Text Field"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              margin: EdgeInsets.all(20),
-              child: TextField(
-                obscureText: true,
-                maxLength: 10,
-                onChanged: (value) {
-                  setState(() {});
-                },
-                controller: controller,
-              ),
-            ),
-            Text(controller.text)
-          ],
-        ),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Latihan Media Query"),),
+      body: Container(
+        width: MediaQuery.of(context).size.width/3,
+        height: MediaQuery.of(context).size.height/2,
+        color: Colors.red,
       ),
     );
   }
