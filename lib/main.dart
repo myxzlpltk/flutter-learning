722253848b3d37a61e3c1bbd29b0880238103d15
Saddam Azy <myxzlpltk@gmail.com>
2020-12-19 15:53:16 +0700

    Learning Card

--- lib/main.dart
+++ lib/main.dart
@@ -9,34 +9,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(
-            Icons.adb,
-            color: Colors.white,
-          ),
-          title: Text(
-            "AppBar Gradiation",
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: [
-            IconButton(icon: Icon(Icons.settings), onPressed: (){}),
-            IconButton(icon: Icon(Icons.exit_to_app), onPressed: (){})
-          ],
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.blue, Colors.purple],
-                begin: FractionalOffset.topLeft,
-                end: FractionalOffset.bottomRight
-              ),
-              image: DecorationImage(
-                image: AssetImage("assets/pattern.png"),
-                fit: BoxFit.none,
-                repeat: ImageRepeat.repeat
-              )
-            ),
+        backgroundColor: Colors.green,
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: ListView(
+            children: [
+              buildCard(Icons.account_box, "Account Box"),
+              buildCard(Icons.adb, "ADB Android"),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Card buildCard(IconData iconData, String text) {
+    return Card(
+      elevation: 5,
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.all(5),
+            child: Icon(iconData, color: Colors.green,),
+          ),
+          Text(text)
+        ],
       ),
     );
   }
