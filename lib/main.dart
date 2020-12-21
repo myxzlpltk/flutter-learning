2029aa55329c8cfc7c60f97de950884829df9615
Saddam Azy <myxzlpltk@gmail.com>
2020-12-21 14:30:11 +0700

    Learning Custom Button

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Latihan Media Query"),),
-      body: Container(
-        width: MediaQuery.of(context).size.width/3,
-        height: MediaQuery.of(context).size.height/2,
-        color: Colors.red,
-      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Latihan Membuat Custom Button"),
+          ),
+          body: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              RaisedButton(
+                onPressed: () {},
+                color: Colors.amber,
+                shape: StadiumBorder(),
+                child: Text('Raised Button'),
+              ),
+              Material(
+                borderRadius: BorderRadius.circular(20),
+                elevation: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                          colors: [Colors.purple, Colors.pink],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter)),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.transparent,
+                    child: InkWell(
+                      splashColor: Colors.amber,
+                      borderRadius: BorderRadius.circular(20),
+                      onTap: (){},
+                      child: Center(
+                        child: Text(
+                          "My Button",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                  ),
+                  width: 150,
+                  height: 40,
+                ),
+              )
+            ],
+          ))),
     );
   }
 }
