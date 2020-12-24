36634a137fcd1da3b107853a340d812d3e7d1617
Saddam Azy <myxzlpltk@gmail.com>
2020-12-25 00:43:15 +0700

    Learning Tab Bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(200),
-          child: AppBar(
-            backgroundColor: Colors.amber,
-            flexibleSpace: Container(
-              alignment: Alignment.bottomRight,
-              margin: EdgeInsets.all(20),
-              child: Text(
-                "AppBar with Custom Height",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700,
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Learning Tab Bar"),
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.home),
+                  text: "Home",
                 ),
-              ),
+                Tab(
+                  icon: Icon(Icons.explore),
+                  text: "Discover",
+                ),
+                Tab(
+                  icon: Icon(Icons.notifications),
+                  text: "Notifications",
+                ),
+                Tab(
+                  icon: Icon(Icons.person),
+                  text: "Profil",
+                ),
+              ],
             ),
+          ),
+          body: TabBarView(
+            children: [
+              Center(child: Text("Tab 1")),
+              Center(child: Text("Tab 2")),
+              Center(child: Text("Tab 3")),
+              Center(child: Text("Tab 4")),
+            ],
           ),
         ),
       ),
