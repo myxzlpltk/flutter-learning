52ad131cc902ad5852d6384de49973e31c89e0ad
Saddam Azy <myxzlpltk@gmail.com>
2020-12-24 22:40:34 +0700

    AppBar with custom height

--- lib/main.dart
+++ lib/main.dart
@@ -9,66 +9,22 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MainPage());
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "Latihan Hero Animation",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      backgroundColor: Colors.lightBlue[50],
-      body: GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context){
-            return SecondPage();
-          }));
-        },
-        child: Hero(
-          tag: 'pp',
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(50),
-            child: Container(
-              width: 100,
-              height: 100,
-              child: Image(
-                fit: BoxFit.cover,
-                image: AssetImage("assets/juan.jpg"),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.pink[50],
-      appBar: AppBar(
-        title: Text("Juan"),
-      ),
-      body: Center(
-        child: Hero(
-          tag: 'pp',
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(100),
-            child: Container(
-              width: 200,
-              height: 200,
-              child: Image(
-                fit: BoxFit.cover,
-                image: AssetImage("assets/juan.jpg"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(200),
+          child: AppBar(
+            backgroundColor: Colors.amber,
+            flexibleSpace: Container(
+              alignment: Alignment.bottomRight,
+              margin: EdgeInsets.all(20),
+              child: Text(
+                "AppBar with Custom Height",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
           ),
