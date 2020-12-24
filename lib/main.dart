52f937fe35088dc0c5e03363c70610e6d5c20bc6
Saddam Azy <myxzlpltk@gmail.com>
2020-12-25 00:55:46 +0700

    Learning QR Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,47 +8,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    TabBar myTabBar = TabBar(
-      indicator: BoxDecoration(
-        color: Colors.orange,
-        border: Border(
-          bottom: BorderSide(
-            color: Colors.red,
-            width: 2,
-          ),
-        ),
-      ),
-      tabs: [
-        Tab(
-          icon: Icon(Icons.home),
-          text: "Home",
-        ),
-        Tab(
-          icon: Icon(Icons.explore),
-          text: "Discover",
-        ),
-      ],
-    );
-
     return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Learning Tab Bar"),
-            bottom: PreferredSize(
-              preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
-              child: Container(
-                color: Colors.yellow,
-                child: myTabBar,
-              ),
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              Center(child: Text("Tab 1")),
-              Center(child: Text("Tab 2")),
-            ],
+      home: Scaffold(
+        body: Center(
+          child: QrImage(
+            version: 6,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            errorCorrectionLevel: QrErrorCorrectLevel.H,
+            data: "Juan Angela Alma",
+            size: 300,
+            padding: EdgeInsets.all(20),
           ),
         ),
       ),
