7277ad700cccdcde9ac684e3f232b65e741eb6dd
Saddam Azy <myxzlpltk@gmail.com>
2020-12-25 16:25:06 +0700

    QR Scan

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,49 @@
-import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  static AudioCache cache = AudioCache();
-  AudioPlayer audioPlayer;
-  String durasi = "00:00:00";
-
-  void playSound() async{
-    if(audioPlayer != null){
-      stopSound();
-    }
-
-    audioPlayer = await cache.play("chun-li.mp3");
-    audioPlayer.onAudioPositionChanged.listen((duration) {
-      setState(() {
-        durasi = duration.toString();
-      });
-    });
-    audioPlayer.setReleaseMode(ReleaseMode.LOOP);
-  }
-
-  void pauseSound() async{
-    await audioPlayer.pause();
-  }
-
-  void resumeSound() async{
-    await audioPlayer.resume();
-  }
-
-  void stopSound() async{
-    await audioPlayer.stop();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Play Music")),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              RaisedButton(
-                onPressed: () {
-                  playSound();
-                },
-                child: Text("Play"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  pauseSound();
-                },
-                child: Text("Pause"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  stopSound();
-                },
-                child: Text("Stop"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  resumeSound();
-                },
-                child: Text("Resume"),
-              ),
-              Text(
-                durasi,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-              )
-            ],
-          ),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  String text = "Hasil QR Scan";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("QR Scan")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(text),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              onPressed: () async {
+                text = await scanner.scan();
+                setState(() {
+
+                });
+              },
+              child: Text("Scan"),
+            ),
+          ],
         ),
       ),
     );
