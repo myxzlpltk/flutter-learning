ea5b0c3671f1c2e71b6399235b76bce5625cb112
Saddam Azy <myxzlpltk@gmail.com>
2020-12-25 21:16:24 +0700

    Animated Padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/post_result_model.dart';
-import 'package:learn_flutter/user_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,35 +10,83 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String output = "no data";
-  PostResult postResult = null;
-  User user = null;
+
+  double myPadding = 5;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("API Demo")),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(output, textAlign: TextAlign.center),
-              RaisedButton(
-                onPressed: () {
-                  User.getUsers("2").then((users) {
-                    setState(() {
-                      output = "";
-                      for (int i = 0; i < users.length; i++) {
-                        output = output + " [ " + users[i].name + " ] ";
-                      }
-                    });
-                  });
-                },
-                child: Text("POST"),
+        appBar: AppBar(title: Text("Animated Padding")),
+        body: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: AnimatedPadding(
+                      duration: Duration(seconds: 1),
+                      padding: EdgeInsets.all(myPadding),
+                      child: GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            if(myPadding == 20){
+                              myPadding = 5;
+                            }
+                            else{
+                              myPadding = 20;
+                            }
+                          });
+                        },
+                        child: Container(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: AnimatedPadding(
+                      duration: Duration(seconds: 1),
+                      padding: EdgeInsets.all(myPadding),
+                      child: Container(
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: AnimatedPadding(
+                      duration: Duration(seconds: 1),
+                      padding: EdgeInsets.all(myPadding),
+                      child: Container(
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: AnimatedPadding(
+                      duration: Duration(seconds: 1),
+                      padding: EdgeInsets.all(myPadding),
+                      child: Container(
+                        color: Colors.yellow,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
