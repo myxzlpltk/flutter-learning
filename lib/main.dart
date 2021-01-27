60e2c89dac5eb1dbd905b7fff07081f49f468c49
Saddam Azy <myxzlpltk@gmail.com>
2021-01-28 02:29:19 +0700

    provider state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
+
+import 'application_color.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,70 +13,52 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-  TextEditingController controller = TextEditingController(text: "No Name");
-  bool isON = false;
-
-  void saveData() async{
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    pref.setString("nama", controller.text);
-    pref.setBool("ison", isON);
-  }
-
-  Future<String> getNama() async{
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    return pref.getString("nama") ?? "No Name";
-  }
-
-  Future<bool> getIsON() async{
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    return pref.getBool("ison") ?? false;
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Shared Preference')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              TextField(
-                controller: controller,
+      home: ChangeNotifierProvider<ApplicationColor>(
+        create: (context) => ApplicationColor(),
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Consumer<ApplicationColor>(
+              builder: (context, applicationColor, _) => Text(
+                "Provider State Management",
+                style: TextStyle(color: applicationColor.color),
               ),
-              Switch(
-                value: isON,
-                onChanged: (newValue){
-                  setState(() {
-                    isON = newValue;
-                  });
-                },
-              ),
-              RaisedButton(
-                child: Text("Save"),
-                onPressed: (){
-                  saveData();
-                },
-              ),
-              RaisedButton(
-                child: Text("Load"),
-                onPressed: (){
-                  getNama().then((value){
-                    setState(() {
-                      controller.text = value;
-                    });
-                  });
-                  getIsON().then((value){
-                    setState(() {
-                      isON = value;
-                    });
-                  });
-                },
-              )
-            ],
+            ),
           ),
-        ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Consumer<ApplicationColor>(
+                  builder: (context, applicationColor, _) => AnimatedContainer(
+                    width: 100,
+                    height: 100,
+                    color: applicationColor.color,
+                    duration: Duration(milliseconds: 500),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(margin: EdgeInsets.all(5), child: Text("AB")),
+                    Consumer<ApplicationColor>(
+                      builder: (context, applicationColor, _) => Switch(
+                        value: applicationColor.isLightBlue,
+                        onChanged: (newValue) {
+                          applicationColor.isLightBlue = newValue;
+                        },
+                      ),
+                    ),
+                    Container(margin: EdgeInsets.all(5), child: Text("LB"))
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
       ),
     );
   }
