cc4815f48f51f1d6a668d663e8cee337c735c0be
Saddam Azy <myxzlpltk@gmail.com>
2021-01-28 08:02:26 +0700

    Custom Progress Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,105 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  bool isStop = true;
-  int counter = 0;
-  Color color = Colors.black;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Timer")),
+        appBar: AppBar(title: Text("Custom Progress Bar")),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                counter.toString(),
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: color
+          child: ChangeNotifierProvider<TimeState>(
+            create: (context) => TimeState(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Consumer<TimeState>(
+                  builder: (context, timeState, _) => CustomProgressBar(
+                    width: 200,
+                    value: timeState.time,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              RaisedButton(
-                child: Text("Direct"),
-                onPressed: (){
-                  Timer.run(() {
-                    setState(() {
-                      if(color == Colors.black)
-                        color = Colors.red;
-                      else
-                        color = Colors.black;
-                    });
-                  });
-                },
-              ),
-              SizedBox(height: 10),
-              RaisedButton(
-                child: Text("Delay"),
-                onPressed: (){
-                  Timer(Duration(seconds: 5), (){
-                    setState(() {
-                      if(color == Colors.black)
-                        color = Colors.red;
-                      else
-                        color = Colors.black;
-                    });
-                  });
-                },
-              ),
-              SizedBox(height: 10),
-              RaisedButton(
-                child: Text("Start Timer"),
-                onPressed: (){
-                  isStop = false;
-                  Timer.periodic(Duration(seconds: 1), (timer) {
-                    if(isStop) timer.cancel();
-
-                    setState(() {
-                      counter += 1;
-                    });
-                  });
-                },
-              ),
-              SizedBox(height: 10),
-              RaisedButton(
-                child: Text("Stop Timer"),
-                onPressed: (){
-                  setState(() {
-                    isStop = true;
-                  });
-                },
-              ),
-            ],
+                SizedBox(height: 10),
+                Consumer<TimeState>(
+                  builder: (context, timeState, _) => RaisedButton(
+                    color: Colors.lightBlue,
+                    child: Text("Start", style: TextStyle(color: Colors.white)),
+                    onPressed: (){
+                      Timer.periodic(Duration(seconds: 1), (timer) {
+                        if(timeState.time == 0) timer.cancel();
+                        else timeState.time -= 1;
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+}
+
+class CustomProgressBar extends StatelessWidget {
+  final double width;
+  final int value;
+  final int totalValue = 15;
+
+  const CustomProgressBar({Key key, this.width, this.value})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double ratio = value / totalValue;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.timer, color: Colors.grey[700]),
+        SizedBox(width: 5),
+        Stack(
+          children: [
+            Container(
+              width: width,
+              height: 10,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+            Material(
+              borderRadius: BorderRadius.circular(5),
+              elevation: 3,
+              child: AnimatedContainer(
+                width: ratio * width,
+                height: 10,
+                duration: Duration(milliseconds: 500),
+                decoration: BoxDecoration(
+                  color: (ratio < 0.3) ? Colors.red : (ratio < 0.6) ? Colors.orange : Colors.green ,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class TimeState with ChangeNotifier{
+  int _time = 15;
+
+  int get time => _time;
+  set time(int value){
+    _time = value;
+    notifyListeners();
   }
 }
