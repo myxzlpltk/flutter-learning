abc5074397d2b74504cf4d7bfa8a773305bb0aa3
Saddam Azy <myxzlpltk@gmail.com>
2021-01-29 08:42:24 +0700

    Flutter 1.9 (SelectableText, ToggleButtons, ColorFiltered)

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/product_card.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<bool> isSelected = [true, false, false];
+  ColorFilter colorFilter = ColorFilter.mode(Colors.blue, BlendMode.screen);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(backgroundColor: firstColor),
-        body: ChangeNotifierProvider<ProductState>(
-          create: (context, ) => ProductState(),
-          child: Container(
-            margin: EdgeInsets.all(20),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Consumer<ProductState>(
-                builder: (context, productState, _) => ProductCard(
-                  name: "Juan",
-                  price: "Rp. 25.000",
-                  quantity: productState.quantity,
-                  notification: (productState.quantity > 5) ? "Diskon 10%" : null,
-                  onAddCartTap: (){
-
-                  },
-                  onIncrementTap: (){
-                    productState.quantity += 1;
-                  },
-                  onDecrementTap: (){
-                    productState.quantity -= 1;
-                  },
+      home: ColorFiltered(
+        colorFilter: colorFilter,
+        child: Scaffold(
+          appBar: AppBar(title: Text("Widgets Demo GDG 2019 China")),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SelectableText(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  showCursor: true,
+                  cursorWidth: 3,
+                  cursorColor: Colors.red,
                 ),
-              ),
+                SizedBox(height: 10),
+                ToggleButtons(
+                  isSelected: isSelected,
+                  children: [
+                    Icon(Icons.access_alarm),
+                    Icon(Icons.adb),
+                    Icon(Icons.add_comment),
+                  ],
+                  onPressed: (index){
+                    setState(() {
+                      isSelected.fillRange(0, isSelected.length, false);
+                      isSelected[index] = !isSelected[index];
+                    });
+                  },
+                  fillColor: Colors.red[50],
+                  selectedColor: Colors.red,
+                  selectedBorderColor: Colors.red,
+                  splashColor: Colors.blue,
+                  highlightColor: Colors.yellow,
+                  borderRadius: BorderRadius.circular(10),
+                )
+              ],
             ),
           ),
         ),
       ),
     );
-  }
-}
-
-class ProductState with ChangeNotifier{
-  int _quantity = 0;
-
-  int get quantity => _quantity;
-  set quantity(value){
-    _quantity = value;
-    notifyListeners();
   }
 }
