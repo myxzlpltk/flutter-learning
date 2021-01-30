0aa3fa28e605abee015f73194a6ad6e7bdb1b6a2
Saddam Azy <myxzlpltk@gmail.com>
2021-01-30 17:52:58 +0700

    hydrated_bloc

---
+++ lib/color_bloc.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+
+enum ColorEvent { toAmber, toLightBlue }
+
+class ColorBloc extends HydratedBloc<ColorEvent, Color>{
+
+  ColorBloc() : super(Colors.amber){
+    hydrate();
+  }
+
+  @override
+  Stream<Color> mapEventToState(ColorEvent event) async* {
+    yield (event == ColorEvent.toAmber) ? Colors.amber : Colors.lightBlue;
+  }
+
+  @override
+  Color fromJson(Map<String, dynamic> json) {
+    try{
+      return Color(json['color'] as int);
+    } catch(_){
+      return null;
+    }
+  }
+
+  @override
+  Map<String, int> toJson(Color state) {
+    try{
+      return {'color': state.value};
+    } catch(_){
+      return null;
+    }
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:learn_flutter/color_bloc.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build();
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  List<bool> isSelected = [true, false, false];
-  ColorFilter colorFilter = ColorFilter.mode(Colors.blue, BlendMode.screen);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ColorFiltered(
-        colorFilter: colorFilter,
-        child: Scaffold(
-          appBar: AppBar(title: Text("Widgets Demo GDG 2019 China")),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SelectableText(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                  showCursor: true,
-                  cursorWidth: 3,
-                  cursorColor: Colors.red,
-                ),
-                SizedBox(height: 10),
-                ToggleButtons(
-                  isSelected: isSelected,
-                  children: [
-                    Icon(Icons.access_alarm),
-                    Icon(Icons.adb),
-                    Icon(Icons.add_comment),
-                  ],
-                  onPressed: (index){
-                    setState(() {
-                      isSelected.fillRange(0, isSelected.length, false);
-                      isSelected[index] = !isSelected[index];
-                    });
-                  },
-                  fillColor: Colors.red[50],
-                  selectedColor: Colors.red,
-                  selectedBorderColor: Colors.red,
-                  splashColor: Colors.blue,
-                  highlightColor: Colors.yellow,
-                  borderRadius: BorderRadius.circular(10),
-                )
-              ],
-            ),
+      home: BlocProvider<ColorBloc>(
+        create: (context) => ColorBloc(),
+        child: MainPage(),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ColorBloc bloc = BlocProvider.of<ColorBloc>(context);
+
+    return Scaffold(
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.amber,
+            onPressed: () {
+              bloc.add(ColorEvent.toAmber);
+            },
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            backgroundColor: Colors.lightBlue,
+            onPressed: () {
+              bloc.add(ColorEvent.toLightBlue);
+            },
+          )
+        ],
+      ),
+      appBar: AppBar(title: Text("BLoc dengan flutter_bloc")),
+      body: Center(
+        child: BlocBuilder<ColorBloc, Color>(
+          builder: (context, color) => AnimatedContainer(
+            width: 100,
+            height: 100,
+            color: color,
+            duration: Duration(milliseconds: 500),
           ),
         ),
       ),
